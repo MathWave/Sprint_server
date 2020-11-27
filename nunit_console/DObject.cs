4e615926d1da1c69cd026e5ce9e6d9cb15fbfdd2
Egor <emmtvv@icloud.com>
2020-11-27 15:52:02 +0300

    playing with docker

---
+++ nunit_console/DObject.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reflection;
+
+namespace Darwin
+{
+    public class DObject
+    {
+
+        private object _thisObject;
+
+        private BindingFlags AllFlags => BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static;
+
+        private MethodInfo GetMethodInfo(string methodName)
+        {
+            return _thisObject.GetType().GetMethod(methodName, AllFlags);
+        }
+
+        public DObject(string exePath, string className)
+        {
+            Assembly ass = Assembly.LoadFrom(exePath);
+            _thisObject = Activator.CreateInstance(ass.GetType(className));
+            if (_thisObject == null)
+                throw new ArgumentException("Assembly or classname is wrong");
+        }
+
+        public object GetObject => _thisObject;
+
+        public object this[string name]
+        {
+            get {
+                var type = _thisObject.GetType();
+                try
+                {
+                    return type.GetField(name, AllFlags).GetValue(_thisObject);
+                }
+                catch
+                {
+                    return type.GetProperty(name, AllFlags).GetValue(_thisObject);
+                }
+            }
+            set {
+                var type = _thisObject.GetType();
+                try
+                {
+                    type.GetProperty(name, AllFlags).SetValue(_thisObject, value);
+                }
+                catch
+                {
+                    type.GetField(name, AllFlags).SetValue(_thisObject, value);
+                }
+            }
+        }
+
+        public object InvokeMethod(string methodName, object[] inputValues = null)
+        {
+            MethodInfo mi = GetMethodInfo(methodName);
+            return mi.Invoke(_thisObject, inputValues);
+        }
+
+    }
+
+}
